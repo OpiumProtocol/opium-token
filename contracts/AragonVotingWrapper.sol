a1d6ff9337da0e12d105a7151f2cff9d2ccf8583
Ali Nuraldin <alirunwwc@gmail.com>
2021-01-15 15:00:46 +0100

    Remove unneccessary ABI encoder pragma declaration

--- contracts/AragonVotingWrapper.sol
+++ contracts/AragonVotingWrapper.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity 0.7.6;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/contracts/token/ERC20/IERC20.sol";
--- contracts/OpiumToken.sol
+++ contracts/OpiumToken.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity 0.7.6;
-pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/contracts/drafts/ERC20Permit.sol";
 
